45daa8a607c9c1c50b3aed35f76d37d785f7ae36
KwangWook Seo <100064247+tommyEzreal@users.noreply.github.com>
2023-06-12 21:29:32 +0900

    Update 0612.py

--- algorithm/programmers/0612.py
+++ algorithm/programmers/0612.py
@@ -9,6 +9,21 @@ def solution(t, p):
         if int(sliced) <= int(p):
             count += 1
     return count
-  
-  
+ 
+    
+    
+    
+# https://school.programmers.co.kr/learn/courses/30/lessons/142086
+
+def solution(s):
+    answer = []
+    last_index = {}
+    for i in range(len(s)):
+        if s[i] not in last_index:
+            answer.append(-1)
+        else:
+            answer.append(i - last_index[s[i]])
+        last_index[s[i]] = i
+    
+    return answer
   
